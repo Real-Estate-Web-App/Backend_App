63532491cadcc0951c68fb352d4a3e1cd83aa2a0
Alexandra Pop <alexandra.pop201@gmail.com>
2023-04-27 02:10:08 +0300

    Added update user functionality

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, abort, jsonify, session
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS, cross_origin
 from flask_session import Session
 from models import db, User
 from config import AppConfig
@@ -10,6 +11,7 @@ app = Flask(__name__)
 app.config.from_object(AppConfig)
 
 bcrypt = Bcrypt(app)
+cors = CORS(app, supports_credentials=True)
 server_session = Session(app)
 db.init_app(app)
 
@@ -17,6 +19,7 @@ with app.app_context():
     # db.drop_all()
     db.create_all()
 
+@cross_origin
 @app.route("/register", methods=["POST"])
 def register_user():
     email = request.json["email"] 
@@ -39,6 +42,7 @@ def register_user():
         "email": new_user.email
     })
 
+@cross_origin
 @app.route("/login", methods=["POST"])
 def login_user():
     email = request.json["email"] 
@@ -52,7 +56,8 @@ def login_user():
     if not bcrypt.check_password_hash(user.password, password):
         return jsonify({"error": "Incorrect password"}), 401
 
-    session["user_id"] = user.id
+    # create a session for the logged user (tot timpul una noua -> o stergem la logout?):
+    # session["user_id"] = user.id
 
     return jsonify({
         "id": user.id,
@@ -60,9 +65,12 @@ def login_user():
         "role": user.role.name
     })
 
-@app.route("/user", methods=["GET"])
+@cross_origin
+@app.route("/user", methods=["POST"])
 def get_logged_user():
-    user_id = session.get("user_id")
+    # user_id = session.get("user_id")
+
+    user_id = request.json["id"]
 
     if not user_id:
         return jsonify({"error": "You're not logged in"}), 401
@@ -74,6 +82,33 @@ def get_logged_user():
         "first_name": user.first_name,
         "last_name": user.last_name
     })
+
+@cross_origin
+@app.route("/logout", methods=["GET"])
+def logout_user():
+    # user_id = session.get("user_id")
+
+    # if user_id:
+        # session.pop("user_id")
+    return "200" # always return 200, but if user is logged in, delete its session
+
+@cross_origin
+@app.route("/updateProfile", methods=["POST"])
+def update_user_profile():
+    user_id = request.json["id"]
+    user_first_name = request.json["first_name"]
+    user_last_name = request.json["last_name"]
+
+    if not user_id:
+        return jsonify({"error": "You're not logged in"}), 401
+
+    user = User.query.filter_by(id=user_id).first()
+    user.first_name = user_first_name
+    user.last_name = user_last_name
+    db.session.add(user)
+    db.session.commit()
+
+    return "200"
 
 if __name__ == "__main__":
     app.run(debug=True)
